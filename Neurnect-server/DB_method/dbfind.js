ffbf46db358087214be1e602a3cef5bf3590b61b
rikyuusima <equanz324@gmail.com>
2016-09-12 17:27:06 +0900

    fix Syntax

--- Neurnect-server/DB_method/dbfind.js
+++ Neurnect-server/DB_method/dbfind.js
@@ -1,64 +1,65 @@
 //全件抽出
 var dball = function() {
   Posted.find({}, function(err, docs) {
-    if(err){ console.log(error); }
+    if(err){ console.log(err); }
     else{
-      return (docs); 
-     }
-}
+      return docs;
+    }
+  });
+};
 
 //全件抽出 IDのみ
 var dballid = function() {
-  Posted.find({}, {'_id'}, function(err, docs){
-    if(err){ console.log(error); }
-    else{ return (docs); }
+  Posted.find({}, ['_id'], function(err, docs){
+    if(err){ console.log(err); }
+    else{ return docs; }
   });
-}
+};
 
 //タグ毎に抽出
 var dbtag = function(tag) {
   Posted.find({ tag: tag }, function(err, docs) {
-    if(err){ console.log(error); }
-    else{ return (docs); }
+    if(err){ console.log(err); }
+    else{ return docs; }
   });
-}
+};
 
-//タグ毎に抽出　IDのみ
+//タグ毎に抽出 IDのみ
 var dbtagid = function(tag) {
-  Posted.find({ tag: tag }, {'_id'}, function(err, docs) {
-    if(err){ console.log(error); }
-    else{ return (docs); }
+  Posted.find({ tag: tag }, ['_id'], function(err, docs) {
+    if(err){ console.log(err); }
+    else{ return docs; }
   });
-}
+};
 
 //各タグに対する投稿数
 var dbtagcount = function(tag) {
   Posted.count({ tag: tag }, function(err, count) {
-    if(err){ console.log(error); }
+    if(err){ console.log(err); }
     else{ return (count); }
   });
-}
+};
 
 //カテゴリ毎に抽出
 var dbcate = function(cate) {
   Posted.find({ cate: cate }, function(err, docs) {
-    if(err){ console.log(error); }
-    else{ return (docs); }
+    if(err){ console.log(err); }
+    else{ return docs; }
   });
-}
+};
 
 //カテゴリ毎に抽出 IDのみ
 var dbcateid = function(cate) {
-  Posted.find({ cate: cate }, {'_id'}, function(err, docs) {
-    if(err){ console.log(error); }
-    else{ return (docs); }
+  Posted.find({ cate: cate }, ['_id'], function(err, docs) {
+    if(err){ console.log(err); }
+    else{ return docs; }
   });
-}
+};
 
 //各カテゴリに対する投稿数
 var dbcatecount = function(cate) {
   Posted.count({ cate: cate }, function(err, count) {
-    if(err){ console.log(error); }
+    if(err){ console.log(err); }
     else{ return (count); }
   });
-}
+};
