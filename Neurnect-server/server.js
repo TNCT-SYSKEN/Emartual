59996fe354f3c8171d40df7a419e823e595cd55a
Liao40 <giwedob.c0037.arual@gmail.com>
2016-09-21 10:48:31 +0900

    add svmodule.js

--- Neurnect-server/server.js
+++ Neurnect-server/server.js
@@ -10,8 +10,6 @@ var mainUI = require("./mainUI.js");
 var DBModule = require("./DB_method/dbmodule.js");
 var SVModule = require("./svmodule.js");
 
-var flag = 0; //初回ページを表示するか判別用の変数
-
 var dbmodule = new DBModule(mongoose);
 
 dbmodule.dbdefine();
@@ -23,14 +21,12 @@ mainUI.mainUI(app); //mainUIの情報を受け取る
 io.sockets.on("connection", function(socket){
   console.log("-------------------------------------------");
   //初回ページの要求
-  if(flag != 1){
-    dbmodule.dball(function(init_data){
-      io.sockets.emit("init_data", init_data);
-      console.log("init_data:");
-      console.log(init_data);
-      console.log("");
-    });
-  }
+  dbmodule.dball(function(init_data){
+    io.sockets.emit("init_data", init_data);
+    console.log("init_data:");
+    console.log(init_data);
+    console.log("");
+  });
   socket.on('upload_data', function(upload_data){
     console.log("upload_data:");
     console.log(upload_data);
@@ -40,7 +36,6 @@ io.sockets.on("connection", function(socket){
       console.log(upload_data);
       console.log("");
     });
-    flag = 1;
 
     dbmodule.dball(function(update_data){
       io.sockets.emit("update_data", update_data);
