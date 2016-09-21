907f8ba97b1147f932d7ffcf66a2d3a1d042da46
Syaz0326 <tadakazu326@gmail.com>
2016-09-21 13:40:47 +0900

    投稿されたデータのemit

--- Neurnect-server/views/js/event.js
+++ Neurnect-server/views/js/event.js
@@ -9,10 +9,8 @@ $('form').submit(function (e){
   socket.emit('upload_data', {
     "text": $("input#uploadtext").val(),
     "form": $("#graphic-form").val(),
-    "image": "",
-    "position": "",
-    "category": "",
-    "tag": $("input#tag").val(),
+    "position": CalcPosition($("input#uploadtext").val(), $("#graphic-form").val(), $("input#tag-select").val()),
+    "tag": $("input#tag-select").val(),
     "link": "",
     "date": new Date()
   });
