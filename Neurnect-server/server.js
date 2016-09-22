2016c84d2dff90f5f92f7694f41b554dc58cc963
Liao40 <giwedob.c0037.arual@gmail.com>
2016-09-22 13:22:13 +0900

    tag情報の受け取りと保存

--- Neurnect-server/server.js
+++ Neurnect-server/server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
+    favicon = require('serve-favicon');
 var ejs = require('ejs');
 var mongoose = require('mongoose');
 
@@ -13,10 +14,13 @@ var SendFiles = require("./SendFiles.js");
 var dbmodule = new DBModule(mongoose);
 
 dbmodule.dbdefine();
+dbmodule.tagdefine();
 
 app.engine('ejs', ejs.renderFile);
 
 mainUI.mainUI(app); //mainUIの情報を受け取る
+
+app.use(favicon(__dirname + '/views/image/favicon.ico'));
 
 io.sockets.on("connection", function(socket){
   //初回ページの要求
@@ -24,10 +28,20 @@ io.sockets.on("connection", function(socket){
     io.sockets.emit("init_data", init_data);
   });
 
+  //DBへtagデータの受け渡しの要求
+  dbmodule.tagall(function(init_tag){
+    io.sockets.emit("init_tag", init_tag);
+  });
+
   socket.on('upload_data', function(upload_data){
     dbmodule.dbinsert(upload_data);
     io.sockets.emit("update_data", upload_data);
   });
+
+  socket.on('upload_tag', function(upload_tag){
+    dbmodule.taginsert(upload_tag);
+  });
+
 });
 
 SendFiles.send_Files(app);
