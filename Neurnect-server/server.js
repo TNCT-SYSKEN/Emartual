4e98a297a741e6e67aa0ec8f727269d98b508fed
Liao40 <giwedob.c0037.arual@gmail.com>
2016-09-22 17:56:34 +0900

    tagデータの送受信

--- Neurnect-server/server.js
+++ Neurnect-server/server.js
@@ -31,17 +31,21 @@ io.sockets.on("connection", function(socket){
   //DBへtagデータの受け渡しの要求
   dbmodule.tagall(function(init_tag){
     io.sockets.emit("init_tag", init_tag);
+    console.log("");
+    console.log(init_tag);
+  });
+
+  socket.on('upload_tag', function(upload_tag){
+    console.log("");
+    console.log(upload_tag);
+    dbmodule.taginsert(upload_tag);
+    io.sockets.emit("update_tag", upload_tag);
   });
 
   socket.on('upload_data', function(upload_data){
     dbmodule.dbinsert(upload_data);
     io.sockets.emit("update_data", upload_data);
   });
-
-  socket.on('upload_tag', function(upload_tag){
-    dbmodule.taginsert(upload_tag);
-  });
-
 });
 
 SendFiles.send_Files(app);
