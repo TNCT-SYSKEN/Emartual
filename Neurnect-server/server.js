c6b293b7d650869f6c9a5c3ac3db824ca125d262
Liao40 <giwedob.c0037.arual@gmail.com>
2016-11-04 14:54:02 +0900

    カテゴリの作成

--- Neurnect-server/server.js
+++ Neurnect-server/server.js
@@ -24,21 +24,19 @@ mainUI.mainUI(app); //mainUIの情報を受け取る
 app.use(favicon(__dirname + '/views/image/favicon.ico'));
 
 io.sockets.on("connection", function(socket){
-
   //初回ページの要求
-  dbmodule.dbcate("normal", function(init_data){  //normalカテゴリーを表示
+  dbmodule.dbcate("cate_name", function(init_data){  //normalカテゴリを表示
     //DBへtagデータの受け渡しの要求
     dbmodule.tagall(function(init_tag){
       for(var i = 0; i < init_tag.length; i++){
         init_tag[i].color = color_settings.color_settings[init_tag[i].color];
       }
-      io.sockets.emit("init", {
+      socket./*broadcast.to().*/emit("init", {
         "data": init_data,
         "tag": init_tag
       });
     });
   });
-
 
   //positonの抽出
   dbmodule.dbposition_x_max(function(position_x){
@@ -56,48 +54,62 @@ io.sockets.on("connection", function(socket){
     });
   });
 
-
-  //tag情報の受け渡し
-  socket.on('upload', function(upload){
-    var propcount = Math.floor(Math.random() * (6 - 0) + 0);
-    var count = 0;
-    var update_tag = {};
-    update_tag.tag = upload.data.tag;
-
-    if(upload.isnewtag){
-      for (var result in color_settings.color_settings){
-        if (propcount == count){
-          update_tag.color = result;
-          break;
+  //カテゴリチャンネルへの参加
+  socket.on("request_category",function(cate_name){
+    socket.join(cate_name); //カテゴリチャンネルに参加
+    dbmodule.dbcate(cate_name,function(cate_data){  //カテゴリデータの抽出
+      dbmodule.tagall(function(cate_tag){ //DBへのTagデータの受け渡し要求
+        for(var i = 0; i < cate_tag.length; i++){
+          cate_tag[i].color = color_settings.color_settings[cate_tag[i].color];
         }
-        count++;
+        socket.to(cate_data).emit("response_category",{
+          "data": cate_data,
+          "tag": cate_tag
+        });
+      });
+    });
+
+
+
+    //tag情報の受け渡し
+    socket.on('upload', function(upload){
+      var propcount = Math.floor(Math.random() * (6 - 0) + 0);
+      var count = 0;
+      var update_tag = {};
+      update_tag.tag = upload.data.tag;
+
+      //新しいタグが投稿されたら
+      if(upload.isnewtag){
+        for (var result in color_settings.color_settings){
+          if (propcount == count){
+            update_tag.color = result;
+            break;
+          }
+          count++;
+        }
+        dbmodule.taginsert({
+          "tag": update_tag.tag,
+          "color": update_tag.color
+        });
       }
-      dbmodule.taginsert({
-        "tag": update_tag.tag,
-        "color": update_tag.color
+      else{
+        dbmodule.tagfindone(update_tag.tag, function(doc){
+          update_tag.color = doc.color;
+        });
+      }
+
+      update_tag.color = color_settings.color_settings[update_tag.color];
+      //upload.dataをDBに渡す
+      dbmodule.dbinsert(upload.data);
+      socket.to(cate_name).emit("update", { //update.dataをフロントへ渡す
+        "data": upload.data,
+        "tag": update_tag
       });
-    }
-    else{
-      dbmodule.tagfindone(update_tag.tag, function(doc){
-        update_tag.color = doc.color;
-      });
-    }
-
-
-    update_tag.color = color_settings.color_settings[update_tag.color];
-
-
-    dbmodule.dbinsert(upload.data);
-    io.sockets.emit("update", {
-      "data": upload.data,
-      "tag": update_tag
     });
   });
 });
 
-
 SendFiles.send_Files(app);
-
 
 server.listen(setting.port, setting.host, function(){
   console.log("server listening...  " + setting.host + ":" + setting.port);
